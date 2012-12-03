45c92c9cdb9111b14d22c22a3511831431395be6
ometz2012 <goldman_alex@walla.com>
2012-12-03 13:05:27 -0800

    Eliya: Site map

--- CinemaCity/Ometz.Cinema.UI/Ometz.Cinema.NewUI/Site.Master.designer.cs
+++ CinemaCity/Ometz.Cinema.UI/Ometz.Cinema.NewUI/Site.Master.designer.cs
@@ -40,6 +40,15 @@ namespace Ometz.Cinema.UI {
         protected global::System.Web.UI.WebControls.Menu NavigationMenu;
         
         /// <summary>
+        /// SiteMapDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SiteMapDataSource SiteMapDataSource1;
+        
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
